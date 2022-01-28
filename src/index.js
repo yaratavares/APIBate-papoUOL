29e64d3c144b49031c22207afbc038f9223937d9
yaratavares <yaracriistavares@gmail.com>
2022-01-27 23:26:50 -0300

    participant route post and get ready

--- src/index.js
+++ src/index.js
@@ -2,6 +2,8 @@ import express, { json } from "express";
 import { MongoClient } from "mongodb";
 import cors from "cors";
 import dotenv from "dotenv";
+import joi from "joi";
+import dayjs from "dayjs";
 dotenv.config();
 
 const app = express();
@@ -10,6 +12,18 @@ app.use(json());
 
 const mongoClient = new MongoClient(process.env.MONGO_URI);
 let db;
+
+const participantSchema = joi.object({
+  name: joi.string().required(),
+});
+
+const messageSchema = joi.object({
+  from: joi.string().required(),
+  to: joi.string().required(),
+  text: joi.string().required(),
+  type: joi.string().required(),
+  time: joi.string(),
+});
 
 mongoClient.connect().then(() => {
   db = mongoClient.db("batepapo_uol");
@@ -29,10 +43,41 @@ app.get("/participants", async (req, res) => {
 });
 
 app.post("/participants", async (req, res) => {
+  const participant = req.body;
+
+  const validation = participantSchema.validate(participant, {
+    abortEarly: false,
+  });
+  if (validation.error) {
+    const err = validation.error.details.map((detail) => detail.message);
+    res.status(422).send(err);
+    return;
+  }
+
+  const participantExist = await db
+    .collection("participant")
+    .find({ name: participant.name })
+    .toArray();
+  if (participantExist.length !== 0) {
+    res.status(409).send("name already exists");
+    return;
+  }
+
   try {
-    const status = await db.collection("participant").insertOne(req.body);
-    if (status) {
-      res.status(201).send(status);
+    const participantStatus = await db
+      .collection("participant")
+      .insertOne({ name: participant.name, lastStatus: Date.now() });
+
+    const enterMessage = await db.collection("messages").insertOne({
+      from: participant.name,
+      to: "Todos",
+      text: "entra na sala...",
+      type: "status",
+      time: dayjs().format("HH:mm:ss").toString(),
+    });
+
+    if (participantStatus) {
+      res.sendStatus(201);
     }
   } catch (err) {
     res.sendStatus(500);
